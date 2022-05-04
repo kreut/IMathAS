e9c0a89223aa7e486720001aa27443e6833702ee
Eric Kean <kreut@hotmail.com>
2022-05-04 14:57:00 -0700

    Returns correct learning tree info

--- adapt/assess2sup.js
+++ adapt/assess2sup.js
@@ -1,5 +1,5 @@
 function adaptSubmitq(qn) {
-    $("#results"+qn).html(_('Submitting...'));
+    $("#results" + qn).html(_('Submitting...'));
     var data = dopresubmit(qn, true);
     data.append('state', document.getElementById('state').value);
     data.append('problemJWT', document.getElementById('problemJWT').value);
@@ -11,39 +11,42 @@ function adaptSubmitq(qn) {
         data: data,
         processData: false,
         contentType: false
-    }).done(function(msg) {
+    }).done(function (msg) {
 
-       let response;
+        let response;
         try {
-           response = JSON.parse(msg);
-        } catch(error) {
-           alert(msg);
+            response = JSON.parse(msg);
+        } catch (error) {
+            alert(msg);
             return false;
         }
-       // var data = parseJwt(msg.jwt);
+        // var data = parseJwt(msg.jwt);
         //$("#state").val(data.state);
         //showerrors(data.errors);
         //if (msg.disp) {
-         //   $("#results"+qn).html(_("Score: ")+data.score);
-          //  showandinit(qn, msg.disp);
+        //   $("#results"+qn).html(_("Score: ")+data.score);
+        //  showandinit(qn, msg.disp);
         //} else {
-         //   $("#results"+qn).html(_('Question Submitted'));
-          //  $("#questionwrap"+qn).empty();
-       // }
+        //   $("#results"+qn).html(_('Question Submitted'));
+        //  $("#questionwrap"+qn).empty();
+        // }
+        console.log(response)
+        var returnobj = {
+            subject: "lti.ext.imathas.result",
+            message: response.message,
+            type: response.type,
+            learning_tree_message: response.learning_tree_message ? 1 : 0,
+            learning_tree: response.learning_tree,
+            traffic_light_color: response.traffic_light_color ? response.traffic_light_color : '',
+            last_submitted: response.last_submitted,
+            student_response: response.student_response,
+            frame_id: frame_id
+        };
+        window.parent.postMessage(JSON.stringify(returnobj), '*');
 
-            var returnobj = {
-                subject: "lti.ext.imathas.result",
-                message: response.message,
-                type: response.type,
-                last_submitted: response.last_submitted,
-                student_response: response.student_response,
-                frame_id: frame_id
-            };
-            window.parent.postMessage(JSON.stringify(returnobj), '*');
-
-    }).always(function(msg) {
+    }).always(function (msg) {
         $("#toscoreqn").val('');
-        $("#results"+qn).html(_(''));
+        $("#results" + qn).html(_(''));
 
     });
 }
